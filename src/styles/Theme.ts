715a080d79608411db809d612ef35b7c918cf4a6
Jairo Vidomines <jvidomines@gmail.com>
2023-03-09 21:02:43 +0100

    Create theme provider

---
+++ src/styles/Theme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  fonts: {
+    primary: "Inter",
+  },
+  colors: {
+    backgroundColor: "#fff",
+    mainColorButton: "#3f5e46",
+    mainFontColor: "#000",
+    mainFontColorButton: "#fff",
+  },
+};
+
+export default theme;
