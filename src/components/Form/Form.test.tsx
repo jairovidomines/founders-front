be87a1463864d9ea57f0d3be8228dc24f9eb7866
Jairo Vidomines <jvidomines@gmail.com>
2023-03-09 20:06:35 +0100

    Test the functionality of the Form component

--- src/components/Form/Form.test.tsx
+++ src/components/Form/Form.test.tsx
@@ -1,12 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { act, fireEvent, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { UserCredentials } from "../../hooks/useUser/types";
+import renderWithProviders from "../../testUtils/renderWithProviders";
 import Form from "./Form";
+
+const mockLoginUser = jest.fn();
+
+jest.mock("../../hooks/useUser/useUser", () => () => ({
+  loginUser: mockLoginUser,
+}));
 
 describe("Given a Form component", () => {
   describe("When it is rendered", () => {
     test("Then it should show an image with alt text: 'founders_logo'", () => {
       const expectResult = "founders_logo";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByAltText(expectResult);
 
@@ -16,7 +25,7 @@ describe("Given a Form component", () => {
     test("Then it should show an input with label text: 'Username'", () => {
       const expectResult = "Username";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByLabelText(expectResult);
 
@@ -26,7 +35,7 @@ describe("Given a Form component", () => {
     test("Then it should show an input with label text: 'Password'", () => {
       const expectResult = "Password";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByLabelText(expectResult);
 
@@ -36,7 +45,7 @@ describe("Given a Form component", () => {
     test("Then it should show a button with text: 'Log in'", () => {
       const expectResult = "Log in";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByRole("button", { name: expectResult });
 
@@ -46,7 +55,7 @@ describe("Given a Form component", () => {
     test("Then it should show the text: 'Not a member?'", () => {
       const expectResult = "Not a member?";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByText(expectResult);
 
@@ -56,11 +65,68 @@ describe("Given a Form component", () => {
     test("Then it should show the text: 'Sign up'", () => {
       const expectResult = "Sign up";
 
-      render(<Form />);
+      renderWithProviders(<Form />);
 
       const result = screen.getByText(expectResult);
 
       expect(result).toBeInTheDocument();
+    });
+  });
+
+  describe("When the user writes on the 'Username' input", () => {
+    test("Then it changes the value of this input", async () => {
+      const expectUsernameLabel = "Username";
+      const expectResult = "Jairo";
+
+      renderWithProviders(<Form />);
+
+      const result = screen.getByLabelText(expectUsernameLabel);
+
+      await act(async () => await userEvent.type(result, expectResult));
+
+      expect(result).toHaveValue(expectResult);
+    });
+  });
+
+  describe("When tue user writes on the 'Password' input", () => {
+    test("Then it changes the value of this input", async () => {
+      const expectPasswordLabel = "Password";
+      const expectResult = "Jairo1020!";
+
+      renderWithProviders(<Form />);
+
+      const result = screen.getByLabelText(expectPasswordLabel);
+
+      await act(async () => await userEvent.type(result, expectResult));
+
+      expect(result).toHaveValue(expectResult);
+    });
+  });
+
+  describe("When the user submit the form", () => {
+    test("Then the loginUser function should be called", async () => {
+      const expectUsernameLabel = "Username...";
+      const expectPasswordLabel = "Password...";
+      const expectSubmitButton = "Log in";
+
+      const mockUser: UserCredentials = {
+        username: "",
+        password: "",
+      };
+
+      renderWithProviders(<Form />);
+
+      const resultUsername = screen.getByPlaceholderText(expectUsernameLabel);
+      const resultPassword = screen.getByPlaceholderText(expectPasswordLabel);
+      const resultSubmit = screen.getByRole("button", {
+        name: expectSubmitButton,
+      });
+
+      fireEvent.change(resultUsername, mockUser.username);
+      fireEvent.change(resultPassword, mockUser.password);
+      fireEvent.click(resultSubmit);
+
+      expect(mockLoginUser).toHaveBeenCalledWith(mockUser);
     });
   });
 });
