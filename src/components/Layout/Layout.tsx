befe984df6bbffb5826dee28010fd477e3698905
Jairo Vidomines <jvidomines@gmail.com>
2023-03-10 22:13:42 +0100

    Add toastify modal of wrong credentials on Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,32 @@
+import { useEffect } from "react";
+import { useAppSelector } from "../../store/hooks";
+
 import LoginPage from "../../pages/LoginPage";
+import { showErrorToast } from "../../modals/modals";
+import { ToastContainer } from "react-toastify";
 
 const Layout = (): JSX.Element => {
-  return <LoginPage />;
+  const { modal } = useAppSelector((state) => state.ui);
+
+  useEffect(() => {
+    if (modal) {
+      showErrorToast(modal);
+    }
+  }, [modal]);
+  return (
+    <>
+      <LoginPage />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick={false}
+        pauseOnHover={false}
+        draggable={true}
+        theme="light"
+      />
+    </>
+  );
 };
 
 export default Layout;
