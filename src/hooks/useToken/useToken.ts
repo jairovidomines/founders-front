d964bbd1a8c76cc630697516921e2330dcdba872
Jairo Vidomines <jvidomines@gmail.com>
2023-03-11 14:10:58 +0100

    Create useToken custom hook

---
+++ src/hooks/useToken/useToken.ts
@@ -0,0 +1,32 @@
+import { useCallback } from "react";
+import { useAppDispatch } from "../../store/hooks";
+import decodeToken from "jwt-decode";
+import { CustomTokenPayload } from "../useUser/types";
+import { loginUserActionCreator } from "../../store/features/userSlice/userSlice";
+
+interface UseTokenStructure {
+  getToken: () => void;
+  removeToken: () => void;
+}
+
+const useToken = (): UseTokenStructure => {
+  const dispatch = useAppDispatch();
+
+  const getToken = useCallback(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const { username, id } = decodeToken<CustomTokenPayload>(token);
+
+      dispatch(loginUserActionCreator({ username, id, token }));
+    }
+  }, [dispatch]);
+
+  const removeToken = () => {
+    localStorage.removeItem("token");
+  };
+
+  return { getToken, removeToken };
+};
+
+export default useToken;
