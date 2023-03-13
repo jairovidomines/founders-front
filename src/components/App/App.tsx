ac5753601408353d89a4bc9bb0a0502b69b90f20
Jairo Vidomines <jvidomines@gmail.com>
2023-03-13 19:16:04 +0100

    Add feature to remember token user

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,11 @@
+import useToken from "../../hooks/useToken/useToken";
 import Layout from "../Layout/Layout";
 
 const App = () => {
+  const { getToken } = useToken();
+
+  getToken();
+
   return (
     <>
       <div className="containeer">
