64eaf8936ec503caaf9fb255ac6a2af8fb68825b
Jairo Vidomines <jvidomines@gmail.com>
2023-03-13 10:03:16 +0100

    Restructure modals

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,24 +2,15 @@ import { Navigate } from "react-router-dom";
 import LoginForm from "../components/LoginForm/LoginForm";
 import { useAppSelector } from "../store/hooks";
 import LoginPageStyled from "./LoginPageStyled";
-import { ToastContainer } from "react-toastify";
 
 const LoginPage = (): JSX.Element => {
   const { isLogged } = useAppSelector((state) => state.user);
+
   return isLogged ? (
     <Navigate to={"/"} replace={true} />
   ) : (
     <LoginPageStyled>
       <LoginForm />
-      <ToastContainer
-        position="top-center"
-        autoClose={3000}
-        hideProgressBar={false}
-        closeOnClick={false}
-        pauseOnHover={false}
-        draggable={true}
-        theme="light"
-      />
     </LoginPageStyled>
   );
 };
