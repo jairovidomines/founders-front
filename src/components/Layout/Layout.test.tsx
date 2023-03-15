fce88305ab16b44c501e90af0fddf0251834bd8d
Jairo Vidomines <jvidomines@gmail.com>
2023-03-15 18:06:34 +0100

    Add tests at Layout to cover all corner cases

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,19 +1,36 @@
 import { screen } from "@testing-library/react";
 import { renderRouterWithProviders } from "../../testUtils/renderWithProviders";
-import Header from "../Header/Header";
+import Layout from "./Layout";
 
 describe("Given a Layout component", () => {
   describe("When it is rendered", () => {
     test("Then it should show a button with text: 'Log in'", () => {
       const expectText = "founders_logo";
 
-      renderRouterWithProviders(<Header />, {
+      renderRouterWithProviders(<Layout />, {
         user: { isLogged: true, id: "", token: "", username: "" },
       });
 
       const result = screen.getByTestId(expectText);
 
       expect(result).toBeInTheDocument();
+    });
+  });
+
+  describe("When it is rendered and isLoading is true", () => {
+    test("Then it should show the Loader component", () => {
+      const areaLabel = "Page loading";
+
+      renderRouterWithProviders(<Layout />, {
+        ui: {
+          isLoading: true,
+          modal: { message: "", isError: true, isSucess: false },
+        },
+      });
+
+      const loader = screen.getByLabelText(areaLabel);
+
+      expect(loader).toBeInTheDocument();
     });
   });
 });
