e63006a8634334ccf01af2e396a4f0e435bf35e6
Jairo Vidomines <jvidomines@gmail.com>
2023-03-16 20:36:34 +0100

    Test deleteProject reducer

--- src/store/features/projectsSlice/projectsSlice.test.ts
+++ src/store/features/projectsSlice/projectsSlice.test.ts
@@ -1,12 +1,20 @@
-import { mockProjectAndroid, mockProjectIos } from "../../../mocks/mocks";
-import { loadProjectsActionCreator, projectsReducer } from "./projectsSlice";
+import {
+  mockProjectAndroid,
+  mockProjectIos,
+  mockProjects,
+} from "../../../mocks/mocks";
+import {
+  deleteProjectActionCreator,
+  loadProjectsActionCreator,
+  projectsReducer,
+} from "./projectsSlice";
 import { ProjectsData, ProjectsDataStructure } from "./types";
 
 const projects: ProjectsDataStructure = [mockProjectAndroid, mockProjectIos];
 const initialProjectsState: ProjectsData = { projects: [] };
 
-describe("Given a user reducer", () => {
-  describe("When it receives a new state an the action to load projects", () => {
+describe("Given a projectsReducer reducer", () => {
+  describe("When it receives a new state and the action to load projects", () => {
     test("Then it should return a list of 2 projects", () => {
       const loadProjectsAction = loadProjectsActionCreator(projects);
 
@@ -18,6 +26,19 @@ describe("Given a user reducer", () => {
       );
 
       expect(newProjects).toStrictEqual(expectedProjectsLoaded);
+    });
+  });
+
+  describe("When it receives a new state and the deleteProject action to delete a project", () => {
+    test("Then it should show an update list without the project deleted", () => {
+      const deleteProjectAction =
+        deleteProjectActionCreator(mockProjectAndroid);
+
+      const expectedProjects: ProjectsData = mockProjects;
+
+      const newProjects = projectsReducer(mockProjects, deleteProjectAction);
+
+      expect(newProjects).toStrictEqual(expectedProjects);
     });
   });
 });
