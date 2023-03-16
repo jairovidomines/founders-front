98810357701aef8bedd137425c3b3ea7b7e16ab0
Jairo Vidomines <jvidomines@gmail.com>
2023-03-16 13:29:27 +0100

    Test PrivatePage

---
+++ src/pages/PrivatePage/PrivatePage.test.tsx
@@ -0,0 +1,17 @@
+import { screen } from "@testing-library/react";
+import { renderWithProviders } from "../../testUtils/renderWithProviders";
+import PrivatePage from "./PrivatePage";
+
+describe("Given a PrivatePage", () => {
+  describe("When it is rendered", () => {
+    test("Then it should show a header with the text: 'My projects'", () => {
+      const expectText = "My Projects";
+
+      renderWithProviders(<PrivatePage />);
+
+      const result = screen.getByRole("heading", { name: expectText });
+
+      expect(result).toBeInTheDocument();
+    });
+  });
+});
