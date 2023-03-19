8f5e329931a43d23e595ee272c11004e54d6c2c2
Jairo Vidomines <jvidomines@gmail.com>
2023-03-19 12:24:28 +0100

    Add styles to page CreatePage

---
+++ src/pages/CreatePage/CreatePageStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const CreatePageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+
+  .create-page__title {
+    font-size: 20px;
+  }
+`;
+
+export default CreatePageStyled;
