0650e3066c3d5d0cc48d4a1c27f3e4b9f16eeead
Jairo Vidomines <jvidomines@gmail.com>
2023-03-21 21:54:58 +0100

    Modify size of avatars on card list section

--- src/components/Card/CardStyled.tsx
+++ src/components/Card/CardStyled.tsx
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 const CardStyled = styled.article`
   display: flex;
-  gap: 15px;
+  gap: 20px;
   padding: 15px 0;
 
   .card {
@@ -13,11 +13,11 @@ const CardStyled = styled.article`
     &__content {
       display: flex;
       flex-direction: column;
-      gap: 14px;
+      gap: 10px;
     }
 
     &__content-short-description {
-      line-height: 1.6;
+      line-height: 1.4;
     }
 
     &__content-name {
